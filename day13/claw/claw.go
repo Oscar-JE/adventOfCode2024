181e2368066113ebb4ea25b384e6f6bcd93f9d5b
Oscar Johannsson Elling <oscar.johansson.elling@gmail.com>
2025-03-06 20:58:11 +0100

    skiss

--- day13/claw/claw.go
+++ day13/claw/claw.go
@@ -1,7 +1,52 @@
 package claw
 
-import "vec2d"
+import (
+	vec "adventofcode/geometry/vec2d"
+)
 
-type Claw struct{
-	a 
+type Button struct {
+	movement vec.Vec2d
+	cost     int
+}
+
+func minimumCost(buttons []Button, prize vec.Vec2d) int {
+	currentPos := vec.Init(0, 0)
+	return minimumCostFromPosition(0, currentPos, buttons, prize)
+}
+
+func minimumCostFromPosition(cost int, pos vec.Vec2d, buttons []Button, prize vec.Vec2d) int {
+	if pos == prize {
+		return cost
+	}
+	nextStepCosts := []int{}
+	for _, b := range buttons {
+		nextPos := vec.Add(pos, b.movement)
+		if !impossibleRoute(nextPos, prize) {
+			nextCost := cost + b.cost
+			nextStepCosts = append(nextStepCosts, minimumCostFromPosition(nextCost, nextPos, buttons, prize))
+		}
+	}
+	return multipleMin(nextStepCosts)
+}
+
+func impossibleRoute(pos vec.Vec2d, prize vec.Vec2d) bool {
+	return !(vec.AbsSquared(pos) > vec.AbsSquared(prize))
+}
+
+func multipleMin(values []int) int {
+	if len(values) == 0 {
+		return -1
+	}
+	minV := values[0]
+	for i := 1; i < len(values); i++ {
+		minV = min(minV, values[i])
+	}
+	return minV
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
