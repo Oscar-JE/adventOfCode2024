e5216b6bff364f7134a4f919d20119d68e640927
Oscar Johannsson Elling <oscar.johansson.elling@gmail.com>
2025-08-14 23:18:38 +0200

    day19 part1

--- day19/main.go
+++ day19/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"adventofcode/day19/towels"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
-	rack, patterns := parseImput("input.txt")
+	rack, patterns := parseInputFile("input.txt")
+	sum := 0
+	for _, p := range patterns {
+		if rack.SolvablePattern(p) {
+			sum++
+		}
+	}
+	fmt.Println(sum)
+}
+
+func parseInputFile(name string) (towels.TowelRack, []towels.Pattern) {
+	bytes, err := os.ReadFile(name)
+	if err != nil {
+		panic("cannot find file")
+	}
+	rep := string(bytes)
+	return parseInput(rep)
+}
+
+func parseInput(rep string) (towels.TowelRack, []towels.Pattern) {
+	splittedRackAndPatterns := strings.Split(rep, "\r\n\r\n")
+	return towels.ParseTowelRack(splittedRackAndPatterns[0]), parsePatterns(splittedRackAndPatterns[1])
+}
+
+func parsePatterns(rep string) []towels.Pattern {
+	ret := []towels.Pattern{}
+	patternReps := strings.Split(rep, "\r\n")
+	for _, rep := range patternReps {
+		ret = append(ret, towels.ParsePattern(rep))
+	}
+	return ret
 }
