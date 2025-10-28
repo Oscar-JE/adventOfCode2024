539b7c177335af2c555b642ad189377fb63441f3
viojog <oscar.johanssonelling@vitecsoftware.com>
2025-10-28 15:49:24 +0100

    Equals för scorelist

--- day20/scorelist/scorelist.go
+++ day20/scorelist/scorelist.go
@@ -26,14 +26,35 @@ func Init(scores []int) ScoreList {
 	return sl
 }
 
+func Add(l1 ScoreList, l2 ScoreList) ScoreList {
+	index1 := 0
+	index2 := 0
+	sumScores := []ScoreAndNumber{}
+	for index1 != len(l1.scores) || index2 != len(l2.scores) {
+		if index1 == len(l1.scores) {
+			sumScores = append(sumScores, l2.scores[index2:]...)
+		}
+		if index2 == len(l2.scores) {
+			sumScores = append(sumScores, l1.scores[index1:]...)
+		}
+		//här händer det lite mer intresanta saker
+		sumScore1 := l1.scores[index1]
+		sumScore2 := l2.scores[index2]
+		if sumScore1.score // fortsätter hät senare
+	}
+	return ScoreList{sumScores}
+}
+
 func (sl ScoreList) Eq(other ScoreList) bool {
 	if len(sl.scores) != len(other.scores) {
 		return false
 	}
-	for i , el := range {
-		
+	for i, el := range sl.scores {
+		if el != other.scores[i] {
+			return false
+		}
 	}
-
+	return true
 }
 
 func (sl ScoreList) findIndexOfScore(score int) (int, bool) {
--- day20/scorelist/scorelist_test.go
+++ day20/scorelist/scorelist_test.go
@@ -1,12 +1,14 @@
 package scorelist
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestInit(t *testing.T) {
 	scores := []int{3, 3, 1, 2}
 	sl := Init(scores)
-	fmt.Println(sl)
+	slExpected := ScoreList{scores: []ScoreAndNumber{{1, 1}, {2, 1}, {3, 2}}}
+	if !slExpected.Eq(sl) {
+		t.Errorf("What what")
+	}
 }
